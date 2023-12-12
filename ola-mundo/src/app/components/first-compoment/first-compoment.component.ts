dde3e1756bce01c97b9912d74b35158b3d9e06bd
Éleson Silva <elesonsilva9@gmail.com>
2023-12-12 11:53:56 -0300

    UPATE

--- ola-mundo/src/app/components/first-compoment/first-compoment.component.ts
+++ ola-mundo/src/app/components/first-compoment/first-compoment.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-compoment',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './first-compoment.component.html',
   styleUrl: './first-compoment.component.css'
 })
-export class FirstCompomentComponent {
-
+export class FirstCompomentComponent{
+  name = 'eleson'
+  idade = '29'
+  profissao = 'front-end'
+  hobbie = ['ler', 'assitir anime', 'tocar Violão']
+  moto ={
+    nome:'pop',
+    potencia:'110cc',
+    ano:'2019'
+  }
 }
