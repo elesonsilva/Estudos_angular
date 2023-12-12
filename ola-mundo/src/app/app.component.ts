3bae4a457bae0e5515ae06554cacf86bab4abe61
Éleson Silva <elesonsilva9@gmail.com>
2023-12-12 14:27:14 -0300

    estudando

--- ola-mundo/src/app/app.component.ts
+++ ola-mundo/src/app/app.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FirstCompomentComponent } from "./components/first-compoment/first-compoment.component";
+import { ParentDataComponent } from "./components/parent-data/parent-data.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, FirstCompomentComponent]
+    imports: [CommonModule, RouterOutlet, FirstCompomentComponent, ParentDataComponent]
 })
 export class AppComponent {
+  userName = 'Rafaely'
+  userData = {
+    email:'rafaely@gmail.com',
+    role:'admin'
+  }
   title = 'ola-mundo';
 }
